dab3ce40bfe7c3426af94047ee80599b6e255072
dany <129038840+xdanyzy@users.noreply.github.com>
2024-10-25 09:58:00 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+let btnOn = document.getElementById('btnaviOn');
+let navi = document.getElementById('NaviShow');
+
+btnOn.addEventListener('click', () => {
+  navi.classList.add('MyNaviClassList');
+});
